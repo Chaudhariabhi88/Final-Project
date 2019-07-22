a273e74db8cb4bf0be5a724d17604e6ab92bb02e
Abhijit <abhijit@Abhijits-iMac.local>
2019-07-22 17:36:37 +0530

    first commit

---
+++ finalProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  finalProject
+//
+//  Created by Abhijit on 22/07/19.
+//  Copyright © 2019 Abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ finalProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  finalProject
+//
+//  Created by Abhijit on 22/07/19.
+//  Copyright © 2019 Abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
